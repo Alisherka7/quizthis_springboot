34a16c053efdb294c0cc5deaeb91ee162922906a
Alisherka7 <alisherka7@jj.ac.kr>
2023-05-02 16:54:12 +0900

    Spring boot Init v0.01

---
+++ src/main/java/alidev/kr/quizthis/QuizthisApplication.java
@@ -0,0 +1,13 @@
+package alidev.kr.quizthis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizthisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizthisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alidev/kr/quizthis/QuizthisApplicationTests.java
@@ -0,0 +1,13 @@
+package alidev.kr.quizthis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizthisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
